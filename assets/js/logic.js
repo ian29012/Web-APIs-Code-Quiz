4049f76bd4b21ff2c74d5cf2922b68a781ff45cb
ian29012 <iancheng29012@gmail.com>
2022-12-14 12:03:43 +0000

    update the var of answer and correctanswer and rewrite the code of function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,18 +7,23 @@ var questionTitle = document.querySelector('#question-title');
 var choices = document.querySelector('#choices');
 var feedBack = document.querySelector('#feedback');
 
-var questionBook = [["You can use the _______ function to find out if an expression (or a variable) is true"],
-                    ["JavaScript _______ are for storing and manipulating text"],
-                    ["________ Represents both integer and floating-point values"]]
+var questionBook = ["You can use the _______ function to find out if an expression (or a variable) is true",
+                    "JavaScript _______ are for storing and manipulating text",
+                    "________ Represents both integer and floating-point values"]
 
 var answerBook = [["Boolean", "Took", "Number", "String"],  //1
                   ["Boolean", "Took", "Number", "String"],  //4
                   ["Boolean", "Took", "Number", "String"]]  //3
 
+var correctAnswer = ["Boolean", "String", "Number"]
 
+// test                
+console.log(questionBook[1])
+console.log(answerBook[0][0])
+console.log(correctAnswer[0])
 
 // time
-timerInput.textContent = 100 ;
+timerInput.textContent = 20 ;
 
 // timer function
 function countDown() {
@@ -30,34 +35,8 @@ function countDown() {
     if ( timerInput.textContent == 0){
         location.href = "highscores.html";
         }
-
-    return;
 }
 
-
-// create a question list
-var ul = document.createElement("ul")
-
-for (var i = 0; i < questionBook.length; i++ ) {
-     var li = document.createElement("li")
-
-     var answerBtn = document.createElement("button");
-     answerBtn.textContent = answerBook[i]
-
-}
-
-function question() {
-
-// go to question1
-   startScreen.setAttribute("class", "hide");
-   questions.setAttribute("class", "start");
-   questionTitle.textContent = questionBook[0];
-   choices.textContent = answerBook[i]
-
-
-   return;
-}
 
 // call the funtion
 addBtn.addEventListener("click", countDown);
-addBtn.addEventListener("click", question);

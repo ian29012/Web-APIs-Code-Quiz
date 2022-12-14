12e2860297bde69363d37a9b5f50e27e95698098
ian29012 <iancheng29012@gmail.com>
2022-12-14 15:52:20 +0000

    updated the Question display

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,36 @@
-// timer element
-var timerInput = document.querySelector('#time');
-var startScreen = document.querySelector('#start-screen');
-var addBtn = document.querySelector('#start');
-var questions = document.querySelector('#questions')
-var questionTitle = document.querySelector('#question-title');
-var choices = document.querySelector('#choices');
-var feedBack = document.querySelector('#feedback');
+// timer section
+var timerInput = document.getElementById("time");
+var startScreen = document.getElementById("start-screen");
+var addBtn = document.getElementById("start");
+// question section
+var questions = document.getElementById("questions")
+var questionTitle = document.getElementById("question-title");
+var choices = document.getElementById("choices");
+var feedBack = document.getElementById("feedback");
+// score section
+var endScreen = document.getElementById("end-screen");
+var score = document.getElementById("final-score");
+var initialsInput = document.getElementById("initials");
+var submitBtn = document.getElementById("submit");
+
 
 var questionBook = ["You can use the _______ function to find out if an expression (or a variable) is true",
                     "JavaScript _______ are for storing and manipulating text",
-                    "________ Represents both integer and floating-point values"]
+                    "________ Represents both integer and floating-point values"];
 
 var answerBook = [["Boolean", "Took", "Number", "String"],  //1
                   ["Boolean", "Took", "Number", "String"],  //4
-                  ["Boolean", "Took", "Number", "String"]]  //3
+                  ["Boolean", "Took", "Number", "String"]];  //3
 
-var correctAnswer = ["Boolean", "String", "Number"]
+var correctAnswer = ["Boolean", "String", "Number"];
 
 // test                
-console.log(questionBook[1])
-console.log(answerBook[0][0])
-console.log(correctAnswer[0])
+console.log(questionBook[0]);
+console.log(answerBook[0][0]);
+console.log(correctAnswer[0]);
 
 // time
-timerInput.textContent = 20 ;
+timerInput.textContent = 100 ;
 
 // timer function
 function countDown() {
@@ -35,8 +42,45 @@ function countDown() {
     if ( timerInput.textContent == 0){
         location.href = "highscores.html";
         }
-}
+};
+
+var startState = startScreen.getAttribute("class")
+var questionState = questions.getAttribute("class")
+
+// Question
+function question(question, answer, correct) {
+
+   // hidden the start page and show the question
+  if ( startState === "start"){
+    startState.class = "hide";
+    startScreen.setAttribute("class", "hide");
+
+    // display question page
+    questionState.class = "show";
+    questions.setAttribute("class", "show");
+
+    //display the question
+    questionTitle.textContent = questionBook[0];
+
+    var loopAnswerOl = document.createElement("ol");
+
+    // use for loop to display the answer
+    for ( var i = 0; i < answerBook[0].length; i++){
+        var loopAnswerLi = document.createElement("li");
+        var answerBtn = document.createElement("Button")
+        answerBtn.textContent = answerBook[0][i];
+        choices.appendChild(loopAnswerOl);
+        loopAnswerOl.appendChild(loopAnswerLi);
+        loopAnswerLi.appendChild(answerBtn);
+      }
+ };
+};
+
+
+
+
 
 
 // call the funtion
 addBtn.addEventListener("click", countDown);
+addBtn.addEventListener("click", question);
